bb24268d7d84c355b3ed8b7251eb291a62147d55
Atharva Khaire <atharvakhaire75@gmail.com>
2025-08-12 00:11:21 +0530

    techniques

--- task1.py
+++ task1.py
@@ -2,11 +2,22 @@ import cv2 as cv
 import numpy as np
 
 img = cv.imread("Images\\CuteCat.jpg")
+#USING KEYWORD
+# gray = cv2.imread("image.jpg", cv2.IMREAD_GRAYSCALE)
 if img is None:
-    print("Error loading the image")
+    print("Error loading the image")        
 else:
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    #USING LIBRARY
+   ''' gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     resized_img = cv.resize(gray, (800,600))
     cv.imshow("Gray", resized_img)
     cv.waitKey(0)
-    cv.destroyAllWindows()
+    cv.destroyAllWindows()'''
+    
+
+# Using average method
+gray = np.mean(img, axis = 2).astype(np.uint8)
+resizeimg = cv.resize(gray, (800,600))
+cv.imshow("Gray Average", resizeimg)
+cv.waitKey(0)
+cv.destroyAllWindows()
