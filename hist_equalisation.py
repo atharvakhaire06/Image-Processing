71d72cdce5c773cc4c9d7fe6831f6d12ccd54a60
Atharva Khaire <atharvakhaire75@gmail.com>
2025-08-13 19:16:59 +0530

    Hist Equalisation

---
+++ hist_equalisation.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread("Images\\originalfaded.jpg")
+if img is None:
+    print("Error Loading in Image")
+else: 
+    ycrcb = cv.cvtColor(img , cv.COLOR_BGR2YCrCb)  # ycrcb works good for colored images
+    y,cr,cb = cv.split(ycrcb)
+    y_eq = cv.equalizeHist(y) 
+
+    ycrcb_eq = cv.merge((y_eq, cr,cb))  #merge with other channels
+
+    y_final = cv.cvtColor(ycrcb_eq , cv.COLOR_YCrCb2BGR)
+    resized_img = cv.resize(y_final, (600, 400))  
+    resizeog = cv.resize(img, (600,400))
+    cv.imshow('Original Image',resizeog)
+    cv.imshow('Enhanced Image',resized_img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
